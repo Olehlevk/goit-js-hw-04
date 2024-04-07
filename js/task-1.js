07dca6c7ac609dfebcd27e24abe03f127d47511f
Olehlevk <154519374+Olehlevk@users.noreply.github.com>
2024-04-07 17:57:09 +0200

    finished

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,16 @@
-function slugify(title) {
-  const sluged = title.split(" ").join("-").toLowerCase();
-  return sluged;
+function isEnoughCapacity(products, containerSize) {
+  let amountOfFruits = 0;
+  const fruits = Object.values(products);
+  for (const fruit of fruits) {
+    amountOfFruits += fruit;
+  }
+  return amountOfFruits <= containerSize;
 }
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
